e5b58a11e7cabcc580a6a0ada8298de7251f66ad
ricarthlima <rrsl@cin.ufpe.br>
2022-10-27 17:54:25 -0300

    v1.3 - Configurando e Instalando o Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'firestore/screens/listin_screen.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
