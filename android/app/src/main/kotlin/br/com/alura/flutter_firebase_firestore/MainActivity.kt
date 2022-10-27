fc085aa8275a910cae7239487e475278d232f9ce
ricarthlima <rrsl@cin.ufpe.br>
2022-10-27 17:52:42 -0300

    flutter project

---
+++ android/app/src/main/kotlin/br/com/alura/flutter_firebase_firestore/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.alura.flutter_firebase_firestore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/firestore/screens/listin_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListinScreen extends StatelessWidget {
+  const ListinScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
