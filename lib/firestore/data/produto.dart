bc40a22d8457b5cf7b705c41a79440ce20f0697c
ricarthlima <rrsl@cin.ufpe.br>
2022-10-28 13:20:23 -0300

    v3.3 - Criando classe Produto

---
+++ lib/firestore/data/produto.dart
@@ -0,0 +1,28 @@
+class Produto {
+  String id;
+  String name;
+  double price;
+  double amount;
+
+  Produto({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.amount,
+  });
+
+  Produto.fromMap(Map<String, dynamic> map)
+      : id = map["id"],
+        name = map["name"],
+        price = map["price"],
+        amount = map["amount"];
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "name": name,
+      "price": price,
+      "amount": amount,
+    };
+  }
+}
